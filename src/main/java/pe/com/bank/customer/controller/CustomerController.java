2faf757332b0904d3b9273286dbe05e7b7c137f5
Erick Pinglo <erick.pinglo@hotmail.com>
2022-05-12 10:53:45 -0500

    "config server"

--- src/main/java/pe/com/bank/customer/controller/CustomerController.java
+++ src/main/java/pe/com/bank/customer/controller/CustomerController.java
@@ -2,6 +2,7 @@ package pe.com.bank.customer.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
+@RefreshScope
 @RestController
 @RequestMapping("/v1")
 public class CustomerController {
